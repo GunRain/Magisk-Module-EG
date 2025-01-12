5a0cf797bcc09e07ad0daaa284984e2779e07bf4
天绾绮奈 <tianwan@oom-wg.dev>
2025-01-12 15:30:18 +0800

    Up

--- mod-root/boot-completed.sh
+++ mod-root/boot-completed.sh
@@ -1,10 +1,10 @@
-baseDir="$(dirname "$(readlink -f "$0")")"
+baseDir="$(get_work_dir "$0")"
 [ -f "$baseDir/skt-utils.sh" ] && . "$baseDir/skt-utils.sh"
 
 # code in boot completed, just supported by KernelSU and APatch, not Magisk, but running this script in service.sh is supported
 
 # code after boot completed and before unlocked
 
-until [ -d /sdcard/Android ]; do sleep 1; done
+until_unlock
 
 # code after unlocked
