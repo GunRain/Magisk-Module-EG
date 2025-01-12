50bdce4f4e8e6364ea273ccd84c39d74b2d09100
天绾绮奈 <tianwan@oom-wg.dev>
2025-01-12 16:47:37 +0800

    Up

--- mod-root/skt-utils.sh
+++ mod-root/skt-utils.sh
@@ -80,6 +80,14 @@ run_bin() {
   eval "\"$file\" $@"
 }
 
+nohup_bin() {
+  file="$1"
+  [ -f "$file" ] || return
+  chmod a+x "$file" 2>/dev/null
+  shift
+  eval "nohup \"$file\" $@ >/dev/null 2>&1 &" &
+}
+
 run_boot_completed_if_magisk() {
   [ "$KSU$APATCH" != true ] && [ -f "$1/boot-completed.sh" ] && { . "$1/boot-completed.sh"; exit; }
 }
