04bfdcf22f9f8c32256b54ff12301f9f25d0e009
天绾绮奈 <tianwan@oom-wg.dev>
2025-01-21 23:25:48 +0800

    Up

--- mod-root/customize.sh
+++ mod-root/customize.sh
@@ -1,6 +1,6 @@
 [ -f "$MODPATH/skt-utils.sh" ] && . "$MODPATH/skt-utils.sh" || abort '! File "skt-utils.sh" does not exist!'
-skt_mod_install # Don't write code before this line!
+skt_install_init # Don't write code before this line!
 
 # code in install
 
-skt_mod_install_finish # Don't write code after this line!
+skt_install_done # Don't write code after this line!
