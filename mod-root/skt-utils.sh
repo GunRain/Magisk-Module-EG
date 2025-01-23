e4a6e8c1361388d6f4fa77f804276f43a55871b0
白彩恋 <shiro@oom-wg.dev>
2025-01-23 20:02:41 +0800

    Up

--- mod-root/skt-utils.sh
+++ mod-root/skt-utils.sh
@@ -4,22 +4,9 @@
 
 alias del=rm # for rm check
 
-skt_abort() {
-  type abort >/dev/null 2>&1 && abort "$@" || { echo "$@"; exit 1; }
-}
-
-get_target_bin() {
-  targetDir="$1"
-  fileName="$2"
-  targetArch="$3"
-  mv -f "$targetDir/$fileName.$targetArch" "$targetDir/$fileName" || skt_abort "! Arch \"$targetArch\" is not supported!"
-  del -f $targetDir/$fileName.*
-  chmod a+x "$targetDir/$fileName"
-}
-
 until_key() {
   while :; do
-    eventCode=`getevent -qlc 1 | awk '{if ($2=="EV_KEY" && $4=="DOWN") {print $3; exit}}'`
+    local eventCode=`getevent -qlc 1 | awk '{if ($2=="EV_KEY" && $4=="DOWN") {print $3; exit}}'`
     case $eventCode in
       KEY_VOLUMEUP) printf up; return;;
       KEY_VOLUMEDOWN) printf down; return;;
@@ -31,7 +18,7 @@ until_key() {
 
 until_key_up_down() {
   while :; do
-    key=`until_key`
+    local key=`until_key`
     case $key in
       up|down) printf $key; return;;
     esac
@@ -40,7 +27,7 @@ until_key_up_down() {
 
 until_key_up_down_power() {
   while :; do
-    key=`until_key`
+    local key=`until_key`
     case $key in
       up|down|power) printf $key; return;;
     esac
@@ -65,8 +52,57 @@ until_key_power() {
   done
 }
 
+goto_url() {
+  [ ! -z "$1" ] || return
+  am start -a android.intent.action.VIEW -d "$1" >/dev/null 2>&1
+}
+
+goto_app() {
+  [ ! -z "$1" ] || return
+  am start "$1" >/dev/null 2>&1
+}
+
+skt_abort() {
+  type abort >/dev/null 2>&1 && abort "! $@" || { echo -e "! $@"; exit 1; }
+}
+
+skt_print() {
+  type ui_print >/dev/null 2>&1 && ui_print "- $@" || echo -e "- $@"
+}
+
+newline() {
+  local method=`type ui_print >/dev/null 2>&1 && printf 'ui_print ""' || printf 'echo ""'`
+  [ -z "$1" ] && { eval "$method"; return; }
+  for _ in $(seq 1 "$1"); do eval "$method"; done
+}
+
 get_work_dir() {
   dirname "`readlink -f "$1"`"
+}
+
+get_target_bin() {
+  local targetDir="$1"
+  local fileName="$2"
+  local targetArch="$3"
+  mv -f "$targetDir/$fileName.$targetArch" "$targetDir/$fileName" || skt_abort "Arch \"$targetArch\" is not supported!"
+  del -f $targetDir/$fileName.*
+  chmod a+x "$targetDir/$fileName"
+}
+
+run_bin() {
+  local file="$1"
+  [ -f "$file" ] || return
+  chmod a+x "$file" 2>/dev/null
+  shift
+  eval "\"$file\" $@"
+}
+
+nohup_bin() {
+  local file="$1"
+  [ -f "$file" ] || return
+  chmod a+x "$file" 2>/dev/null
+  shift
+  eval "nohup \"$file\" $@ >/dev/null 2>&1 &" &
 }
 
 until_boot() {
@@ -78,22 +114,6 @@ until_unlock() {
   until_boot
   until [ -d /sdcard/Android ]; do sleep 1; done
   [ -z "$1" ] || sleep "$1"
-}
-
-run_bin() {
-  file="$1"
-  [ -f "$file" ] || return
-  chmod a+x "$file" 2>/dev/null
-  shift
-  eval "\"$file\" $@"
-}
-
-nohup_bin() {
-  file="$1"
-  [ -f "$file" ] || return
-  chmod a+x "$file" 2>/dev/null
-  shift
-  eval "nohup \"$file\" $@ >/dev/null 2>&1 &" &
 }
 
 is_ksu() {
@@ -123,14 +143,14 @@ set_system_file() {
 }
 
 skt_install_init() {
-  [ -z "$MODPATH" ] && skt_abort '! Value "MODPATH" does not exist!'
+  [ -z "$MODPATH" ] && skt_abort 'Value "MODPATH" does not exist!'
 
   # Check files
-  hashListFile="$MODPATH/hashList.dat"
-  [ -f "$hashListFile" ] || skt_abort '! File "hashList.dat" does not exist!'
-  hashList="`cat "$hashListFile" | zcat | base64 -d`"
+  local hashListFile="$MODPATH/hashList.dat"
+  [ -f "$hashListFile" ] || skt_abort 'File "hashList.dat" does not exist!'
+  local hashList="`cat "$hashListFile" | zcat | base64 -d`"
   for file in $(find "$MODPATH/" -type f -not -path '*META-INF*' -not -name hashList.dat); do
-    [ "$(echo -n "$hashList" | grep -E " ${file#$MODPATH/}$" | awk '{print $1}')" = "$(sha1sum "$file" | awk '{print $1}')" ] || skt_abort "! Failed to verify file \"${file#$MODPATH/}\"!"
+    [ "$(echo -n "$hashList" | grep -E " ${file#$MODPATH/}$" | awk '{print $1}')" = "$(sha1sum "$file" | awk '{print $1}')" ] || skt_abort "Failed to verify file \"${file#$MODPATH/}\"!"
   done
   del -f "$hashListFile"
 
@@ -139,8 +159,8 @@ skt_install_init() {
 }
 
 skt_install_done() {
-  [ -z "$MODPATH" ] && skt_abort '! Value "MODPATH" does not exist!'
-  [ -z "$ARCH" ] && skt_abort '! Value "ARCH" does not exist!'
+  [ -z "$MODPATH" ] && skt_abort 'Value "MODPATH" does not exist!'
+  [ -z "$ARCH" ] && skt_abort 'Value "ARCH" does not exist!'
 
   # For overlyfs
   [ -d "$MODPATH/system" ] && {
