09c0313fae80368d80604e4a24c5ad986aeaf99a
白彩恋 <shiro@oom-wg.dev>
2025-03-01 17:39:47 +0800

    Up

--- root/post-fs-data.sh
+++ root/post-fs-data.sh
@@ -1,4 +1,4 @@
 baseDir="$(dirname "$(readlink -f "$0")")"
-[ -f "$baseDir/skt-utils.sh" ] && . "$baseDir/skt-utils.sh" || exit
+[ -f "$baseDir/nga-utils.sh" ] && . "$baseDir/nga-utils.sh" || exit
 
 # code in post-fs-data
--- root/post-mount.sh
+++ root/post-mount.sh
@@ -1,4 +1,4 @@
 baseDir="$(dirname "$(readlink -f "$0")")"
-[ -f "$baseDir/skt-utils.sh" ] && . "$baseDir/skt-utils.sh" || exit
+[ -f "$baseDir/nga-utils.sh" ] && . "$baseDir/nga-utils.sh" || exit
 
 # code in post-mount, just supported by KernelSU and APatch, not Magisk
